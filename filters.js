66271f4f5b273f1a16f8b9ed0ce3764621289dfb
NF @ NVIDIA Shield TV <info@neue-farben.com>
2013-01-01 01:07:48 +0000

    corrected grid aspect ratio and rotation center

--- filters.js
+++ filters.js
@@ -1912,29 +1912,27 @@ canvas.rainbow=function(size, angle) {
  * @description    Adds a grid to the image
  */
 canvas.grid=function(size, angle, x, y) {
-    x=x|0;
-    y=y|0;
     gl.grid = gl.grid || new Shader(null, '\
         uniform sampler2D texture;\
-      	uniform float size;\
-      	uniform float angle;\
+        uniform vec2 size;\
+        uniform float angle;\
         uniform vec2 offset;\
         varying vec2 texCoord;\
         void main() {\
             vec4 color = texture2D(texture, texCoord);\
-            vec2 uv=(texCoord-offset)*vec2(size,size);\
-            uv=vec2(cos(angle)*uv.x+sin(angle)*uv.y,-sin(angle)*uv.x+cos(angle)*uv.y);\
+            vec2 uv=(texCoord-vec2(0.5,0.5))*size;\
+            uv=vec2(cos(angle)*uv.x+sin(angle)*uv.y,-sin(angle)*uv.x+cos(angle)*uv.y)+offset;\
             \
-            if     (fract(uv.x*8.+.02)<.04 || fract(uv.y*8.+.02)<.04)\
-	            gl_FragColor = vec4(0.0,0.0,0.0,1.0);\
-            else if(fract(uv.x*8.+.05)<.1 || fract(uv.y*8.+.05)<.1)\
-	            gl_FragColor = vec4(1.0,1.0,1.0,1.0);\
+            if     (fract(uv.x+.02)<.04 || fract(uv.y+.02)<.04)\
+                    gl_FragColor = vec4(0.0,0.0,0.0,1.0);\
+            else if(fract(uv.x+.05)<.1 || fract(uv.y+.05)<.1)\
+                    gl_FragColor = vec4(1.0,1.0,1.0,1.0);\
             else\
-	            gl_FragColor = color;\
+                    gl_FragColor = color;\
         }\
     ');
 
-    this.simpleShader( gl.grid, {size: size, angle:angle, offset:[x,y]
+    this.simpleShader( gl.grid, {size: [size*10.,size/this.width*this.height*10.], angle:angle, offset:[x,y]
     });
 
     return this;
