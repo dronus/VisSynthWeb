9cbab83267076bb5911ae0d42024467ec8dc6b91
Joshua Johanan <josh359@gmail.com>
2013-11-16 19:12:36 -0500

    updated grunt and regrunted

---
+++ GruntFile.js
@@ -0,0 +1,52 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'jshint'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {fx: true}
+      },
+      files: ['grunt.js', 'src/**/video.js']
+    },
+    uglify: {
+        dist:{
+            options: {
+                banner: '<%= meta.banner %>'
+            },
+            files: {
+                'dist/WebCamVidja.min.js': ['src/**/video.js']
+            }
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'uglify']);
+
+};
