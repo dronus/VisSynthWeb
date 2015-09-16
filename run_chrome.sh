ff9cc88f1324a66af12370b414713273f711c478
dronus <paul.geisler@web.de>
2015-09-16 18:00:13 +0200

    Unnütze Dateien und Verzeichnisse entfernt, notwendige verschoben und umbenannt

--- run_chrome.sh
+++ run_chrome.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-DISPLAY=:0 chromium-browser --incognito --kiosk --use-gl=egl --use-fake-ui-for-media-stream  http://127.0.0.1:8082/demo/index.html
+DISPLAY=:0 chromium-browser --incognito --kiosk --use-gl=egl --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nodejs central.js
+nodejs server.js
