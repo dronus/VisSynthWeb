cb88f3e11673d488ce98d74df9c0df24f987b5a9
dronus <paul.geisler@web.de>
2015-09-17 02:53:08 +0200

    effekte dazu

--- glfx.js
+++ glfx.js
@@ -322,6 +322,7 @@ exports.canvas = function() {
     canvas.kaleidoscope = wrap(kaleidoscope);
     canvas.tile = wrap(tile);
     canvas.denoise = wrap(denoise);
+    canvas.denoisefast = wrap(denoisefast);    
     canvas.curves = wrap(curves);
     canvas.swirl = wrap(swirl);
     canvas.ink = wrap(ink);
@@ -925,6 +926,52 @@ function denoise(exponent) {
 
     return this;
 }
+
+// src/filters\adjust\denoise.js
+/**
+ * @filter         Denoise
+ * @description    Smooths over grainy noise in dark images using an 9x9 box filter
+ *                 weighted by color intensity, similar to a bilateral filter.
+ * @param exponent The exponent of the color intensity difference, should be greater
+ *                 than zero. A value of zero just gives an 9x9 box blur and high values
+ *                 give the original image, but ideal values are usually around 10-20.
+ */
+function denoisefast(exponent) {
+    // Do a 9x9 bilateral box filter
+    gl.denoisefast = gl.denoisefast || new Shader(null, '\
+        uniform sampler2D texture;\
+        uniform float exponent;\
+        uniform float strength;\
+        uniform vec2 texSize;\
+        varying vec2 texCoord;\
+        void main() {\
+            vec4 center = texture2D(texture, texCoord);\
+            vec4 color = vec4(0.0);\
+            float total = 0.0;\
+            for (float x = -1.0; x <= 1.0; x += 1.0) {\
+                for (float y = -1.0; y <= 1.0; y += 1.0) {\
+                    vec4 sample = texture2D(texture, texCoord + vec2(x, y) / texSize);\
+                    float weight = 1.0 - abs(dot(sample.rgb - center.rgb, vec3(0.25)));\
+                    weight = pow(weight, exponent);\
+                    color += sample * weight;\
+                    total += weight;\
+                }\
+            }\
+            gl_FragColor = color / total;\
+        }\
+    ');
+
+    // Perform five iterations for stronger results
+    for (var i = 0; i < 5; i++) {
+        simpleShader.call(this, gl.denoisefast, {
+            exponent: Math.max(0, exponent),
+            texSize: [this.width, this.height]
+        });
+    }
+
+    return this;
+}
+
 
 // src/filters\adjust\huesaturation.js
 /**
