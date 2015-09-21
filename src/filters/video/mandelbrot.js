df45b7b6e43a9efc80c294f09d150ba5f8157fd7
dronus <paul.geisler@web.de>
2015-09-21 03:39:02 +0200

    mandelbrot with sliding iterations

--- src/filters/video/mandelbrot.js
+++ src/filters/video/mandelbrot.js
@@ -1,27 +1,26 @@
-function mandelbrot(center,scale,angle,iterations) {
+function mandelbrot(center,scale,angle,iterations_float) {
 
-    iterations=Math.min(15,Math.round(iterations));
+    iterations_float=Math.min(15,Math.abs(iterations_float));
+    
+    var iterations=Math.floor(iterations_float);
     gl.mandelbrot=gl.mandelbrot || {};
     gl.mandelbrot[iterations] = gl.mandelbrot[iterations] || new Shader(null, '\
         uniform sampler2D texture;\
         uniform vec4 xform;\
         uniform vec2 center;\
+        uniform float iterations_fract; \
         varying vec2 texCoord;\
         mat2 mat=mat2(xform.xy,xform.zw);\
         void main() {\
-            vec2 position=mat*(texCoord-center);\
-            float x = position.x; \
-            float y = position.y; \
-            float xx = 0.0; \
-            float yy = 0.0; \
+            vec2 c=mat*(texCoord-center);\
+            vec2 z; \
+            vec2 nz=c; \
             for (int iter = 0;iter < '+iterations+';iter++){ \
-              float xx2 = xx*xx; \
-              float yy2 = yy*yy; \
-              float temp = xx2 - yy2 + x; \
-              yy = 2.0*xx*yy + y; \
-              xx = temp; \
+              z = nz; \
+              nz = vec2(z.x*z.x-z.y*z.y, 2.0*z.x*z.y) + c ; \
             } \
-            gl_FragColor = texture2D(texture, vec2(xx,yy)/8.0+vec2(0.5,0.5));\
+            vec2 pos=mix(z,nz,iterations_fract);\
+            gl_FragColor = texture2D(texture, pos/8.0+vec2(0.5,0.5));\
         }\
     ');
 
@@ -30,6 +29,7 @@ function mandelbrot(center,scale,angle,iterations) {
            Math.cos(angle)*scale, Math.sin(angle)*scale,
           -Math.sin(angle)*scale, Math.cos(angle)*scale
         ],
+        iterations_fract: iterations_float-iterations,
         center: [center[0]-this.width/2,center[1]-this.height/2], // TODO remove this fix to cope with UI values top-left origin
         texSize: [this.width, this.height]
     });
