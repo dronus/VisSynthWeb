951bdac746b132f1dab3319968b9d05db90f3ff2
dronus <paul.geisler@web.de>
2015-09-29 21:52:38 +0200

    added aspect correction to transform filter

--- glfx.js
+++ glfx.js
@@ -463,9 +463,14 @@ function transform(x,y,scale,angle) {
         uniform vec2 translation;\
         uniform vec4 xform;\
         varying vec2 texCoord;\
+        uniform vec2 aspect;\
         void main() {\
           mat2 mat=mat2(xform.xy,xform.zw);\
-          gl_FragColor = texture2D(texture, mat*(texCoord+translation));\
+          vec2 uv=(mat*(texCoord*aspect+translation-vec2(0.5,0.5))+vec2(0.5,0.5))/aspect; \
+          if(uv.x>=0. && uv.y>=0. && uv.x<=1. && uv.y<=1.) \
+            gl_FragColor = texture2D(texture,uv);\
+          else \
+            gl_FragColor = vec4(0.,0.,0.,0.); \
         }\
     ');
     
@@ -474,9 +479,10 @@ function transform(x,y,scale,angle) {
     simpleShader.call(this, gl.transform, {
       translation: [x,y],
       xform: [
-         Math.cos(angle)*scale, Math.sin(angle)*scale,
-        -Math.sin(angle)*scale, Math.cos(angle)*scale
-      ]
+         Math.cos(angle)/scale, Math.sin(angle)/scale,
+        -Math.sin(angle)/scale, Math.cos(angle)/scale
+      ],
+      aspect:[this.width/this.height,1.]
     });
 
     return this;
