3192ffb33201a363df5e7f17050f3d262dd50884
dronus <paul.geisler@web.de>
2015-09-29 22:36:07 +0200

    blend_alpha uses alpha channel instead of third input now

--- glfx.js
+++ glfx.js
@@ -170,26 +170,21 @@ var randomShaderFunc = '\
 // src/filters/video/blend_alpha.js
 function blend_alpha() {
     gl.blend_alpha = gl.blend_alpha || new Shader(null, '\
-        uniform sampler2D texture_alpha;\
         uniform sampler2D texture1;\
         uniform sampler2D texture2;\
         varying vec2 texCoord;\
         void main() {\
             vec4 color1 = texture2D(texture1, texCoord);\
             vec4 color2 = texture2D(texture2, texCoord);\
-            float alpha = dot(texture2D(texture_alpha,texCoord).rgb,vec3(1./3.));\
-            gl_FragColor = mix(color1, color2, alpha);\
+            gl_FragColor = mix(color1, color2, color2.a);\
         }\
     ');
 
     var texture1=this.stack_pop();
-    var texture2=this.stack_pop();
     texture1.use(1);
-    texture2.use(2);    
-    gl.blend_alpha.textures({texture_alpha: 0, texture1: 1, texture2: 2});
+    gl.blend_alpha.textures({texture1: 0, texture1: 1});
     simpleShader.call(this, gl.blend_alpha, {});
     texture1.unuse(1);
-    texture2.unuse(2);
 
     return this;
 }
