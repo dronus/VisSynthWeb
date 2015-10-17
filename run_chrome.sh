1c912a79970d5b280c1add0c0c5830d59d1a9ebb
NF @ Brix <info@neue-farben.com>
2015-10-17 02:09:41 +0200

    run_chrome.sh: tasksel raus, chrome statt chromium

--- run_chrome.sh
+++ run_chrome.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-DISPLAY=:0 taskset -c '4-7' chromium-browser --incognito --kiosk --ignore-gpu-blacklist --use-gl=egl --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
+DISPLAY=:0 google-chrome --incognito --kiosk --ignore-gpu-blacklist --use-gl=egl --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
 #DISPLAY=:0 chromium-browser --incognito --use-gl=egl --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
