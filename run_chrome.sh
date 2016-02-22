ed8ad130b41b53a184a57e0f0475dd819af4fda2
dronus <paul.geisler@web.de>
2016-02-22 20:57:25 +0100

    run_chrome.sh now starts chrome with --disable-web-security to allow foreign origin media urls

--- run_chrome.sh
+++ run_chrome.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-DISPLAY=:0 google-chrome --kiosk --incognito  --ignore-gpu-blacklist  --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
+DISPLAY=:0 google-chrome --kiosk --disable-web-security --incognito  --ignore-gpu-blacklist  --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
 #DISPLAY=:0 chromium-browser --incognito --use-gl=egl --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
