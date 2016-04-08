934edd7b854ae931fb6697b443c05a2cb03f4026
NF @ Odroid XU4 2 <info@neue-farben.com>
2016-04-08 22:44:05 +0000

    hard restart triggers xrandr output detection

--- run.sh
+++ run.sh
@@ -5,6 +5,4 @@ cd `dirname $0`
 ./run_server.sh &
 ./run_hardware.sh &
 ./run_chrome.sh &
-# hide mousepointer
-DISPLAY=:0 xdotool mousemove 10000 10000
 
--- run_chrome.sh
+++ run_chrome.sh
@@ -3,5 +3,11 @@
 #DISPLAY=:0 chromium-browser --kiosk --incognito --use-gl=egl --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
 # DISPLAY=:0 chromium-browser --kiosk --disable-web-security --disable-namespace-sandbox --incognito --use-gl=egl --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
 killall chromium-browser
+# make sure the HDMI is enabled
+DISPLAY=:0 xrandr --output HDMI-1 --off
+DISPLAY=:0 xrandr --output HDMI-1 --auto
 sleep 1
+# hide mousepointer
+DISPLAY=:0 xdotool mousemove 10000 10000
+# run browser
 DISPLAY=:0 chromium-browser --kiosk --disable-namespace-sandbox --incognito --use-gl=egl --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
