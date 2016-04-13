6854ab5b8a815e3163ef5b7ca6ae2d1197d4e216
NF @ NVIDIA Shield TV <info@neue-farben.com>
2016-04-13 15:54:15 +0000

    system files tuning for Shield

--- run_chrome.sh
+++ run_chrome.sh
@@ -2,5 +2,9 @@
 #DISPLAY=:0 google-chrome --kiosk --disable-web-security --incognito  --ignore-gpu-blacklist  --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
 #DISPLAY=:0 chromium-browser --incognito --use-gl=egl --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
 DISPLAY=:0 chromium-browser --kiosk --incognito --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
+#DISPLAY=:0 chromium-browser  --incognito --use-fake-ui-for-media-stream  http://127.0.0.1:8082/index.html
 # hide mousepointer
 DISPLAY=:0 xdotool mousemove 10000 10000
+# disable screen saver
+DISPLAY=:0 xset s off
+
