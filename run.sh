d9853bc10eb216eba5156fd26ae4fe441d04b741
Phillip Klein <phillip@neue-farben.com>
2020-02-12 21:15:42 +0100

    Add run script for target platform

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+syncthing -no-browser -home=$HOME/selfiebox/st_config &
+./selfiebox &
+
+xset s off
+xset -dpms
+
+cd `dirname $0`
+export DISPLAY=:0
+xdotool mousemove 1080 1920
+chromium-browser --kiosk --app-auto-launched --app="http://127.0.0.1:1337" --window-position=0,0 --user-data-dir=chromium_tmp --disable-pinch --incognitio --use-fake-ui-for-media-stream
