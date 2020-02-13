422f3da9ace510a80eb6462e55c9a2969ab2bc19
Phillip Klein <phillip@neue-farben.com>
2020-02-13 23:10:43 +0100

    Fix run script

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ xset -dpms
 cd `dirname $0`
 export DISPLAY=:0
 xdotool mousemove 1080 1920
-chromium-browser --kiosk --app-auto-launched --app="http://127.0.0.1:1337" --window-position=0,0 --user-data-dir=chromium_tmp --disable-pinch --incognitio --use-fake-ui-for-media-stream
+chromium-browser --kiosk --app-auto-launched --app="http://127.0.0.1:1337" --window-position=0,0 --user-data-dir=chromium_tmp --disable-pinch --incognito --use-fake-ui-for-media-stream --disable-gpu-sandbox --remote-debugging-port=9222
