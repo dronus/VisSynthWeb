1b2478ec96395c939642c15001711131580eb4e3
NF @ nf-jetson-nano <info@neue-farben.com>
2020-02-14 17:32:36 +0100

    tuning of chroma key, mirrored image

--- run.sh
+++ run.sh
@@ -9,4 +9,5 @@ xset -dpms
 cd `dirname $0`
 export DISPLAY=:0
 xdotool mousemove 1080 1920
+v4l2-ctl  -c zoom_absolute=115
 chromium-browser --kiosk --app-auto-launched --app="http://127.0.0.1:1337" --window-position=0,0 --user-data-dir=chromium_tmp --disable-pinch --incognito --use-fake-ui-for-media-stream --disable-gpu-sandbox --remote-debugging-port=9222
