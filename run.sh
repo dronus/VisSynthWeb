24ba34539ceb38bf85e3faecfa137c7b990bdafe
NF @ nf-jetson-nano <info@neue-farben.com>
2020-02-29 23:20:09 +0100

    Hide cursor the hard way

--- run.sh
+++ run.sh
@@ -11,6 +11,8 @@ xset -dpms
 cd `dirname $0`
 export DISPLAY=:0
 xdotool mousemove 1080 1920
+xsetroot -cursor emptycursor emptycursor
+
 chromium-browser --kiosk --app-auto-launched --app="http://127.0.0.1:1337" --window-position=0,0 --user-data-dir=chromium_tmp --disable-pinch --incognito --use-fake-ui-for-media-stream --disable-gpu-sandbox --remote-debugging-port=9222 &
 sleep 20
 v4l2-ctl -c backlight_compensation=0
