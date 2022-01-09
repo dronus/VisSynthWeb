f317a676d6e5acd9273bdf4456dac88d039ff1ed
dronus <paul.geisler@web.de>
2022-01-09 17:34:35 +0100

    harden set_mode.sh (linux screen resolution setter) for shell parameter escape

--- set_mode.sh
+++ set_mode.sh
@@ -3,6 +3,6 @@
 
 output=`xrandr | head -n 2 | tail -n1 | grep -Eo '^[^ ]+ '`
 echo "Setting output $output to mode $1"
-DISPLAY=:0 xrandr --output $output  --mode $1
+DISPLAY=:0 xrandr --output $output  --mode "$1"
 
 
